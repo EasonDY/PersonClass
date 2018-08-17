6a7c3967e136f0c4ac8ba5cfcd502fbb01e16f13
Dreamy <dreamy9dy@gmail.com>
2018-08-17 23:43:02 +0800

    Finally!!!!

---
+++ PersonClass/Person.h
@@ -0,0 +1,20 @@
+#ifndef PERSON_H
+#define PERSON_H
+#include <string>
+using namespace std;
+
+class Person
+{
+public:
+	Person() {}
+	Person(string nameS, string addressS) : name(nameS), address(addressS) {}
+	~Person() {}
+	string getName() const { return this->name; }
+	string getAddress() const { return this->address; }
+
+private:
+	string name;
+	string address;
+};
+
+#endif // !1
